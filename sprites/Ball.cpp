0657ef3c8272d4ff6fa0f5d054bd555a514fd2b4
Philip Gonzalez <pgonzalez115@gmail.com>
2020-09-08 15:55:11 -0600

    Update ball collision logic

--- sprites/Ball.cpp
+++ sprites/Ball.cpp
@@ -20,25 +20,43 @@ void Ball::Draw()
 void Ball::Update( const Paddle &p1Paddle, const Paddle &p2Paddle, const float dt )
 {
     position += velocity * dt;
-    if ( CheckPaddleCollision( p1Paddle ) || CheckPaddleCollision( p2Paddle ) ) { velocity.x = -velocity.x; }
+
+    HandlePaddleCollision( p1Paddle, position.x, position.x + Constants::BallWidth, position.y, position.y + Constants::BallHeight );
+    HandlePaddleCollision( p2Paddle, position.x, position.x + Constants::BallWidth, position.y, position.y + Constants::BallHeight );
+    HandleWallCollision();
+    HandleGoal();
+}
+void Ball::HandleWallCollision()
+{
+    if ( position.y >= Constants::WindowHeight || position.y <= 0 ) { velocity.y = -velocity.y; }
 }
 
-bool Ball::CheckPaddleCollision(const Paddle &paddle)
+void Ball::HandleGoal()
 {
-    float ballLeft = position.x;
-    float ballRight = position.x + Constants::BallWidth;
-    float ballTop = position.y;
-    float ballBottom = position.y + Constants::BallHeight;
+    if ( position.x >= Constants::WindowWidth || position.x <= 0 )
+    {
+        velocity.y = 0.0f;
+        velocity.x = 1.0f;
+        position.x = ( Constants::WindowWidth / 2.0f ) - ( Constants::BallWidth / 2.0f );
+        position.y = ( Constants::WindowHeight / 2.0f ) - ( Constants::BallHeight / 2.0f );
+    }
+}
+
+void Ball::HandlePaddleCollision( const Paddle &paddle, const float left, const float right, const float top, const float bottom )
+{
 
     float paddleLeft = paddle.position.x;
     float paddleRight = paddle.position.x + Constants::PaddleWidth;
     float paddleTop = paddle.position.y;
     float paddleBottom = paddle.position.y + Constants::PaddleHeight;
 
-    if ( ballLeft >= paddleRight ) { return false; }
-    if ( ballRight <= paddleLeft ) { return false; }
-    if ( ballTop >= paddleBottom ) { return false; }
-    if ( ballBottom <= paddleTop ) { return false; }
+    if ( left < paddleRight && right > paddleLeft && top < paddleBottom && bottom > paddleTop )
+    {
+        float ballYCenter = ( top + bottom ) / 2;
+        float paddleYCenter = ( paddleTop + paddleBottom ) / 2;
+        float ballPaddleYDelta = ballYCenter - paddleYCenter;
 
-    return true;
+        velocity.x = -velocity.x;
+        velocity.y = ( ballPaddleYDelta / ( 1 + ballPaddleYDelta ) ) * ( ballPaddleYDelta > 0 ? 1 : -1 );
+    }
 }
--- sprites/Ball.h
+++ sprites/Ball.h
@@ -8,14 +8,16 @@
 class Ball
 {
 public:
-	Ball( Vec2 position, Vec2 velocity, SDL_Renderer* renderer, int diameter );
-	void Draw();
-	void Update( const Paddle &p1Paddle, const Paddle &p2Paddle, const float dt );
+    Ball( Vec2 position, Vec2 velocity, SDL_Renderer* renderer, int diameter );
+    void Draw();
+    void Update( const Paddle &p1Paddle, const Paddle &p2Paddle, const float dt );
 
-	Vec2 position;
-	Vec2 velocity;
-	SDL_Renderer* renderer;
-	SDL_Rect rect;
+    Vec2 position;
+    Vec2 velocity;
+    SDL_Renderer* renderer;
+    SDL_Rect rect;
 private:
-	bool CheckPaddleCollision(const Paddle &paddle);
+    void HandlePaddleCollision( const Paddle &paddle, const float left, const float right, const float top, const float bottom );
+    void HandleWallCollision();
+    void HandleGoal();
 };
